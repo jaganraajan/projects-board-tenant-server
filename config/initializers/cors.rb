f1fd6138a1e9ce389995661c449d9884b8f5f648
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-07-29 13:09:26 -0400

    update cors config to allow credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,6 +18,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'http://localhost:3000', 'https://projects-board-zeta.vercel.app'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :patch, :put, :delete, :options, :head],
+        credentials: true
     end
   end
