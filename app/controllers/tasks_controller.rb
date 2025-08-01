a2a0884701bed2c60ba48e8e1f1dc92f6cb1baa2
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-08-01 12:55:38 -0400

    revert changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,7 @@ class TasksController < ApplicationController
   end
 
   def task_update_params
-    params.require(:task).permit(:status)
+    params.require(:task).permit(:title, :description, :status)
   end
 
   def task_json(task)
