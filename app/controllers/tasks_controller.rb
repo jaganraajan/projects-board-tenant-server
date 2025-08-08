0711222d815521e0b51ee92ce2edd1abb20ff49e
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-08-07 20:08:04 -0400

    add due_date field to db schema

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,11 +55,11 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :description, :status)
+    params.require(:task).permit(:title, :description, :status, :due_date)
   end
 
   def task_update_params
-    params.require(:task).permit(:title, :description, :status)
+    params.require(:task).permit(:title, :description, :status, :due_date)
   end
 
   def task_json(task)
@@ -68,6 +68,7 @@ class TasksController < ApplicationController
       title: task.title,
       description: task.description,
       status: task.status,
+      due_date: task.due_date,
       user_id: task.user_id,
       created_at: task.created_at,
       updated_at: task.updated_at
