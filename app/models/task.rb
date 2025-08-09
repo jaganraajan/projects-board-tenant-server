d2008288e2211ead4e312456a19dca310ab90521
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-08-09 09:55:31 -0400

    add validation and scope to priority field in task

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   
   validates :title, presence: true
   validates :status, presence: true, inclusion: { in: %w[todo in_progress done] }
-  
+  validates :priority, inclusion: { in: %w[Priority\ 1 Priority\ 2 Priority\ 3 Priority\ 4], allow_blank: true }
+
   scope :by_status, ->(status) { where(status: status) }
+  scope :by_priority, ->(priority) { where(priority: priority) }
 end
